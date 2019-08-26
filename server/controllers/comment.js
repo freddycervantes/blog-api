ee30cf7dd24005336de912052970e6c8d076317f
freddycervantes <freddycervantes@berkeley.edu>
2019-08-25 21:28:05 -0700

    Finished some basic fucntionality for the API

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -14,9 +14,58 @@ class Comments {
                 articleId
             })
             .then(comment => res.status(201).send({
-                message: `Your Article with the title ${title} has been created successfully `,
+                message: `Comment posted successfully `,
                 comment
             }))
+    }
+    static listComments(req, res) {
+        return Comment
+            .findAll()
+            .then(comments => res.status(200).send(comments));
+    }
+
+    static modifyComment(req, res) {
+        const { author, approved, content } = req.body;
+        return Comment
+            .findByPk(req.params.commentId)
+            .then((comment) => {
+                comment.update({
+                    author: author || comment.author,
+                    approved: approved || comment.approved,
+                    content: content || comment.content,
+                })
+                .then((updatedComment) => {
+                    res.status(200).send({
+                        message: 'Comment updated successfully',
+                        data: {
+                            author: author || updatedComment.author,
+                            approved: approved || updatedComment.approved,
+                            content: content || updatedComment.content,
+                        }
+                    })
+                })
+                .catch(error => res.status(400).send(error));
+        })
+        .catch(error => res.status(400).send(error));
+    }
+
+    static deleteComment(req, res) {
+        return Comment
+            .findByPk(req.params.commentId)
+            .then(comment => {
+                if(!comment) {
+                    return res.status(400).send({
+                        message: 'Book Not Found',
+                    });
+                }
+                return comment
+                    .destroy()
+                    .then(() => res.status(200).send({
+                        message: 'Book successfully deleted'
+                    }))
+                    .catch(error => res.status(400).send(error));
+            })
+            .catch(error => res.status(400).send(error))
     }
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,9 @@ export default (app) => {
     app.get('/api', (req, res) => res.status(200).send({
         message: 'Welcome to the Article API!',
     }));
-
+    app.delete('/api/comments/:commentId', Comments.deleteComment); // API route for deleting a comment
+    app.put('/api/comments/:commentId', Comments.modifyComment); // API route for to edit comment
+    app.get('/api/comments', Comments.listComments); // API route for user to get all comments in the database
     app.post('/api/articles', Articles.createArticle); // API route for creating an article
-    app.post('/api/articles/:articleId/comments', Comments.createComment); // API route for user to create a book
+    app.post('/api/articles/:articleId/comments', Comments.createComment); // API route for creating an comment articles
 };
